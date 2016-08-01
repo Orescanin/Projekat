107af2667ccb4f6d16e8cd230b28e7f7519bd6fd
RCSmed03 <RCSmed03>
2016-08-01 12:22:40 +0200

    nov fajl

---
+++ Projekat.py
@@ -0,0 +1,96 @@
+def main():
+
+main()
+niz=[[],[],[]]
+
+
+	x_kordinata=0
+	y_kordinata=0
+
+def dodaj_iks_ili_oks(x_kordinata,y_kordinata):
+
+	 for x_kordinata and y_kordinata in range(0,2):
+	 	print
+	 	
+	 	
+	 	
+def proveri_pobedu:
+
+	 niz=[
+	 ['x','o','x'],
+
+		['o','o','x'],
+
+		['o','x','x']
+
+		]
+
+	if niz[0][0] == niz[0][1] == niz[0][2]:
+		if niz[0][0]=='x'
+			print "prvi igrac pobedio!"
+
+		else 
+			print "drugi igrac pobedio!" 
+
+
+	elif niz[1][0] == niz[1][1] == niz[1][2]:
+		if niz[1][0]=='x'
+			print "prvi igrac pobedio!"
+
+		else 
+			print "drugi igrac pobedio!" 
+
+
+
+	elif niz[2][0] == niz[2][1] == niz[2][2]:
+		if niz[2][0]=='x'
+			print "prvi igrac pobedio!"
+
+		else 
+			print "drugi igrac pobedio!" 
+
+
+	elif niz[0][0] == niz[1][0] == niz[2][0]:
+		if niz[0][0]=='x'
+			print "prvi igrac pobedio!"
+
+		else 
+			print "drugi igrac pobedio!" 
+
+
+	elif niz[0][1] == niz[1][1] == niz[2][1]:
+		if niz[0][1]=='x'
+			print "prvi igrac pobedio!"
+
+		else 
+			print "drugi igrac pobedio!" 
+		
+
+
+	elif niz[0][2] == niz[1][2] == niz[2][2]:
+		if niz[0][2]=='x'
+			print "prvi igrac pobedio!"
+
+		else 
+			print "drugi igrac pobedio!" 
+
+
+	elif niz[0][0] == niz[1][1] == niz[2][2]:
+		if niz[0][0]=='x'
+			print "prvi igrac pobedio!"
+
+		else 
+			print "drugi igrac pobedio!" 
+
+
+	elif niz[0][2] == niz[1][1] == niz[2][0]:
+		if niz[0][2]=='x'
+			print "prvi igrac pobedio!"
+
+		else 
+print "drugi igrac pobedio!" 
+
+
+
+
+
