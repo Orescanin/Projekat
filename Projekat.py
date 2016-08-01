51fb68fc15d56b64ee9ac35c7e7004804a677dbd
RCSmed16 <RCSmed16>
2016-08-01 11:26:02 +0200

    pfff

--- Projekat.py
+++ Projekat.py
@@ -8,13 +8,22 @@ niz=[[],[],[]]
 	y_kordinata=0
 
 def dodaj_iks_ili_oks(x_kordinata,y_kordinata):
+
 	 for x_kordinata and y_kordinata in range(0,2):
 	 	print
 	 	
 	 	
 	 	
 def proveri_pobedu:
-	if niz=[[x,o,x]]:
-		[o,x,x]
-		[x,o,x]
+
+	 niz=[
+	 ['x','o','x'],
+
+		['o','o','x'],
+
+		['o','x','x']
+
+		]
+
+	if niz([0],[0])
 		
