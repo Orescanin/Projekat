4b47ede1341478b35a8542e669f313066b199d1d
RCSmed16 <RCSmed16>
2016-08-01 11:10:13 +0200

    pff

--- Projekat.py
+++ Projekat.py
@@ -3,10 +3,10 @@ def main():
 main()
 niz=[[],[],[]]
 
-def __init__(self,x_kordinata,y_kordinata):
-	self.x_kordinata=x_kordinata
-	self.y_kordinata=y_kordinata
 
-def dodaj_iks_ili_oks(self,x_kordinata,y_kordinata):
+	x_kordinata=0
+	y_kordinata=0
+
+def dodaj_iks_ili_oks(x_kordinata,y_kordinata):
 	 for x_kordinata and y_kordinata in range(0,2):
 	 	print
