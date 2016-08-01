6c6e9d1849b08f731238efebe2cadc9d47d24a37
RCSmed03 <RCSmed03>
2016-08-01 11:12:53 +0200

    Proveri f-ja

--- Projekat.py
+++ Projekat.py
@@ -10,3 +10,10 @@ def __init__(self,x_kordinata,y_kordinata):
 def dodaj_iks_ili_oks(self,x_kordinata,y_kordinata):
 	 for x_kordinata and y_kordinata in range(0,2):
 	 	print
+
+
+def proveri_pobedu:
+	if niz=[[x,o,x]]:
+			[o,x,x]
+			[x,o,x]
+		
