4530761e38e392709abb5990215e8e7f2660a923
RCSmed03 <RCSmed03>
2016-08-01 12:19:19 +0200

    hfhsjh

--- Projekat.py
+++ Projekat.py
@@ -1,19 +1,15 @@
-def main():
+print ("Prvi igrac je na potezu. Upisi koordinate za X(_,_)")
+a = input().split(' ')
+print (a)
+for i in range(0,3):
+	print (niz[i])
 
-main()
-niz=[[],[],[]]
-
-def __init__(self,x_kordinata,y_kordinata):
-	self.x_kordinata=x_kordinata
-	self.y_kordinata=y_kordinata
-
-def dodaj_iks_ili_oks(self,x_kordinata,y_kordinata):
-	 for x_kordinata and y_kordinata in range(0,2):
-	 	print
+print ("Sada je red na drugog igraca. Upisi koordinate za О(_,_).")	
+b = input().split(' ')
+print (b)
+for i in range(0,3):
+	print (niz[i])
 
 
-def proveri_pobedu:
-	if (x_kordinata,y_kordinata)=[[(0,0),(0,1),(0,2)],[(1,0),(1,1),(1,2)],[(2,0),(2,1),(2,2)],[(0,0),(0,1),(0,2)],[(1,0),(1,1),(1,2)],[(2,0),(2,1),(2,2)],[(0,0),(1,1),(2,2)],[(0,2),(2,1),(2,2)]:
-		print "Pobeda"
-	else:
-		print"Izgubio si"
+
+
