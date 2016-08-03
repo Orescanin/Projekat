514cfb8e6a91e8178f5820e24332d2af962dfbad
RCSmed16 <RCSmed16>
2016-08-03 11:05:15 +0200

    a

--- Projekat.py
+++ Projekat.py
@@ -1,16 +1,13 @@
 def main():
 
-main()
-niz=[[1,1,1],[1,1,1],[1,1,1]]
+	niz=[[1,1,1],[1,1,1],[1,1,1]]
 
 
 	x_kordinata=0
 	y_kordinata=0
+main()
 
-def dodaj_iks_ili_oks(x_kordinata,y_kordinata):
 
-	 for x_kordinata and y_kordinata in range(0,2):
-	 	print
 	 	
 	 	
 	 	
