d121ecd2abb79c40d07f2b087ea902188e2a7128
Sophia123456 <sofijadj@verat.net>
2016-08-03 11:21:04 +0200

    Update Projekat.py

--- Projekat.py
+++ Projekat.py
@@ -113,15 +113,17 @@ def proveri_pobedu():
 	else: 
 		print ("Prvi igrac je na potezu. Upisi koordinate za X(_,_)")
 		a = input().split(' ')
-		print (a)
-		for i in range(0,3):
-			print (niz[i])
+		if a!=b:
+			niz.append(a)
+			for i in range(0,3):
+				print (niz[i])
 
 		print ("Sada je red na drugog igraca. Upisi koordinate za О(_,_).")	
 		b = input().split(' ')
-		print (b)
-		for i in range(0,3):
-			print (niz[i])
+		if b!=a:
+			niz.append(b)
+			for i in range(0,3):
+				print (niz[i])
 
 while True:
 	proveri_koordinate()
